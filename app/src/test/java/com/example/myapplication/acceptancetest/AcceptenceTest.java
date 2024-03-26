4bf56c7c0e618124ea9cd8279be27426278a64ae
Aaryaman Pol <Aaryamaanpol15@gmail.com>
2024-03-26 15:38:41 -0500

    "Starting acceptance tests"

---
+++ app/src/test/java/com/example/myapplication/acceptancetest/AcceptenceTest.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.acceptancetest;
+
+import com.example.myapplication.presentation.HomePageActivity;
+
+import androidx.test.core.app.ActivityScenario;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(UITest.class)
+public class AcceptenceTest {
+
+}

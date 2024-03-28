a5201eb1151cfc18fb252625d4c22810a279e137
Aaryaman Pol <Aaryamaanpol15@gmail.com>
2024-03-27 20:00:32 -0500

    "Changes "

--- app/src/androidTest/java/com/example/myapplication/AllSystemTests.java
+++ app/src/androidTest/java/com/example/myapplication/AllSystemTests.java
@@ -1,10 +1,6 @@
 package com.example.myapplication;
 
-import androidx.test.core.app.ActivityScenario;
-import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.example.myapplication.persistence.utils.DBHelper;
-import com.example.myapplication.presentation.LoginActivity;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
